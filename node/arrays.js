e968ae4b1ce893c7419b52fd6689917c621c8b85
twglm9 <tachogardiner@gmail.com>
2019-02-06 20:38:02 -0800

    arrays in javascript

--- node/arrays.js
+++ node/arrays.js
@@ -15,15 +15,30 @@ array[1] = 'hello';
 // for practice
 
 // use forEach
+const arrayTest = [9, 3, 4];
+arrayTest[3] = 9;
+arrayTest.forEach((n) => {
+    console.log(n);
+});
 
 // use map
+const newArray = arrayTest.map(i => i + 1);
+console.log(newArray);
 
 // use pop
+newArray.pop();
+console.log(newArray);
 
 // use push
+newArray.push(6);
+console.log(newArray);
 
 // use shift
+newArray.shift();
+console.log(newArray);
 
 // use unshift
+newArray.unshift(5);
+console.log(newArray);
 
 // use filter
